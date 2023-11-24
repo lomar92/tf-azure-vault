cc24a7d2ecf40efcdc706610741a03173571b23c
Amar Lojo <amarlojo@gmail.com>
2023-11-24 17:04:04 +0100

    workspace-datasource

--- tf-workspace/tf-ws-variable.tf
+++ tf-workspace/tf-ws-variable.tf
@@ -1,52 +1,11 @@
-resource "tfe_organization" "lomar" {
-  name  = "lomar"
-  email = "amar@hashicorp.com"
-}
+## instead use data source for retrieve variables?
+## https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/data-sources/variables 
 
-resource "tfe_workspace" "master" {
+data "tfe_workspace" "master" {
   name         = "tf-azure-vault"
-  organization = tfe_organization.lomar.name
+  organization = "lomar"
 }
 
-resource "tfe_variable" "arm_subscription_id" {
-  key          = "arm_subscription_id"
-  value        = "9d2046c6-eae4-4a64-89f6-94d549f7bdbf"
-  category     = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-}
-
-resource "tfe_variable" "arm_tenant_id" {
-  key          = "arm_tenant_id"
-  value        = "416f7473-ca1a-4528-a4b8-6807af357039"
-  category     = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-}
-
-resource "tfe_variable" "resource_group_name" {
-  key          = "resource_group_name"
-  value        = "vault"
-  category     = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-}
-
-resource "tfe_variable" "vault_azure_secret_backend_path" {
-  key = "vault_azure_secret_backend_path"
-  value = "azure"
-  category = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-}
-
-resource "tfe_variable" "vault_azure_secret_backend_role" {
-  key = "vault_azure_secret_backend_role"
-  value = "my-role"
-  category = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-}
-
-resource "tfe_variable" "vault_token" {
-  key          = "vault_token"
-  value        = "hvs-token"
-  category     = "terraform"
-  workspace_id = "ws-hC4yRRjYepNV2ADs"
-  sensitive    = true
+data "tfe_variables" "test" {
+  workspace_id = data.tfe_workspace.master.id
 }
